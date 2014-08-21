972a8951340dd36e9f47774bb29aa0adff9b9d32
MD.MAHMUDUL HAQUE KHAN <bappy392@gmail.com>
2014-08-21 16:23:58 +0600

    java pograme
    java

---
+++ Activity_two_part3/gen/com/example/activity_two_part3/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.activity_two_part3;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Activity_two_part3/src/com/example/activity_two_part3/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.activity_two_part3;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	Button button1,button2;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		button1=(Button)findViewById(R.id.button1);
+		button2=(Button)findViewById(R.id.button2);
+		
+		button1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(getApplicationContext(), "Button1", Toast.LENGTH_LONG).show();
+				
+			}
+		});
+		
+     button2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(getApplicationContext(), "Button2", Toast.LENGTH_LONG).show();
+				
+			}
+		});
+		
+		
+		
+	}
+	
+	
+
+
+
+}
